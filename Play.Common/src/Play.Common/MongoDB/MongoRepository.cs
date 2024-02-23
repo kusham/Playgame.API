f01ae827d40f922595c1de4abde96f7035aa2a0b
kusham <kusham1998@gmail.com>
2024-02-23 17:43:49 +0530

    create nuget package

---
+++ Play.Common/src/Play.Common/MongoDB/MongoRepository.cs
@@ -0,0 +1,65 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Play.Common
+{
+    public class MongoRepository<T> : IRepository<T> where T : IEntity
+    {
+        private readonly IMongoCollection<T> _mongoCollection;
+        private readonly FilterDefinitionBuilder<T> _filterBuilder = Builders<T>.Filter;
+
+        public MongoRepository(IMongoDatabase mongoDatabase, string collectionName)
+        {
+            _mongoCollection = mongoDatabase.GetCollection<T>(collectionName);
+        }
+
+        public async Task<IReadOnlyCollection<T>> GetAllAsync()
+        {
+            return await _mongoCollection.Find(_filterBuilder.Empty).ToListAsync();
+        }
+
+        public async Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _mongoCollection.Find(filter).ToListAsync();
+        }
+
+        public async Task<T> GetEntityByIdAsync(Guid id)
+        {
+            FilterDefinition<T> filter = _filterBuilder.Eq(entity => entity.Id, id);
+            return await _mongoCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task<T> GetEntityByIdAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _mongoCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateEntityAsync(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            await _mongoCollection.InsertOneAsync(entity);
+        }
+
+        public async Task UpdateEntityAsync(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            FilterDefinition<T> filter = _filterBuilder.Eq(existingEntity => existingEntity.Id, entity.Id);
+            await _mongoCollection.ReplaceOneAsync(filter, entity);
+        }
+
+        public async Task RemoveEntityAsync(Guid id)
+        {
+            FilterDefinition<T> filter = _filterBuilder.Eq(entity => entity.Id, id);
+            await _mongoCollection.DeleteOneAsync(filter);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 ```powershell
 dotnet new webapi -n Play.Catalog.Service
 dotnet add package MongoDB.Driver
+dotnet add package Microsoft.Extensions.Configuration
+dotnet add package Microsoft.Extensions.Configuration.Binder
+dotnet add package Microsoft.Extensions.DependencyInjection
 ```
 
 ## Docker For the service
@@ -14,4 +17,14 @@ docker run -d --rm --name mongo -p 27017:27017 -v mongodbdata:/data/db mongo
 ## See what ports are currently in use
 ```powershell
  Get-NetTCPConnection -State Listen
+```
+
+## Create the Common Service
+```powershell
+dotnet new classlib -n Play.Common --framework net5.0
+```
+
+## Create a Nuget package
+```powershell
+dotnet pack -o ../../../packages
 ```
